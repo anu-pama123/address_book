000707d319b883a9003bdd0335efaaa0bdb4a956
anupama cv <anu.anupamacv@gmail.com>
2021-05-26 22:05:32 +0530

    ADDED: functionality to add a person in address book

---
+++ src/main/java/com/bridgelabz/AddressBook.java
@@ -0,0 +1,25 @@
+package com.bridgelabz;
+import java.util.Scanner;
+public class AddressBook
+{
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String state;
+    String emailId;
+    String zipCode;
+    String phoneNumber;
+    AddressBook(String firstName, String lastName, String address,  String city, String state, String emailId, String zipCode, String phoneNumber )
+    {
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.address=address;
+        this.city=city;
+        this.state=state;
+        this.emailId=emailId;
+        this.zipCode=zipCode;
+        this.phoneNumber=phoneNumber;
+    }
+
+}
--- src/main/java/com/bridgelabz/AddressBookMain.java
+++ src/main/java/com/bridgelabz/AddressBookMain.java
@@ -1,8 +1,41 @@
 package com.bridgelabz;
 
-public class AddressBookMain {
+import java.util.Scanner;
+public class AddressBookMain extends AddressBook
+{
+    AddressBookMain(String firstName, String lastName, String address,  String city, String state, String emailId, String zipCode, String phoneNumber ){
+        super(firstName, lastName, address, city, state, emailId, zipCode, phoneNumber);
+    }
     public static void main(String[] args)
     {
-        System.out.println("Welcome to address book program");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter first name");
+        String firstName = scan.next();
+        System.out.println("Enter last name");
+        String lastName = scan.next();
+        System.out.println("Enter your address");
+        String address = scan.next();
+        System.out.println("Enter your city");
+        String city = scan.next();
+        System.out.println("Enter your state");
+        String state = scan.next();
+        System.out.println("Enter your email");
+        String email = scan.next();
+        System.out.println("Enter your zip code");
+        String zipCode = scan.next();
+        System.out.println("Enter your Phone number");
+        String phoneNumber = scan.next();
+        System.out.println("ADDED DETAILS");
+        AddressBookMain object=new AddressBookMain(firstName, lastName, address, city, state, email, zipCode, phoneNumber);
+        System.out.println("FirstName : " + object.firstName);
+        System.out.println("LastName : " + object.lastName);
+        System.out.println("Address : " + object.address);
+        System.out.println("City : " + object.city);
+        System.out.println("State : " + object.state);
+        System.out.println("EmailId : " + object.emailId);
+        System.out.println("ZipCode : " + object.zipCode);
+        System.out.println("Phone Number : " + object.phoneNumber);
+
     }
+
 }
